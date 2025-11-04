93a7376ba84e238f9dc4fed7ef705ab53f3c8f99
Olga Garcia <olgarciaq11@gmail.com>
2025-11-04 09:26:50 +0100

    precio

--- components/shared/header/product/product-card.tsx
+++ components/shared/header/product/product-card.tsx
@@ -3,28 +3,34 @@ import { Product } from "@/types/Product";
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="p-0 items-center">
         <Link href={`/product/${product.slug}`} passHref>
-          <Image 
-            src={product.images[0]} 
-            alt={product.name} 
-            height={300} 
-            width={300} 
-            priority />
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority
+          />
         </Link>
       </CardHeader>
       <CardContent className="p-4 grid gap-4">
         <div className="text-s">{product.brand}</div>
         <Link href={`/product/${product.slug}`}>
-            <div className="text-sm font-medium">{product.name}</div>
+          <div className="text-sm font-medium">{product.name}</div>
         </Link>
         <div className="flex-between gap-4">
-            <p>{product.rating} Stars</p>
-            <p>{product.stock > 0 ? product.price : "Out of stock"}</p>
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <ProductPrice value={product.price} />
+          ) : (
+            "Out of stock"
+          )}
         </div>
       </CardContent>
     </Card>
--- components/shared/header/product/product-price.tsx
+++ components/shared/header/product/product-price.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { cn } from "@/lib/utils";
 
-export default function ProductPrice() {
+export default function ProductPrice({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) {
+  const stringValue = value.toFixed(2);
+  const [entera, decimales] = stringValue.split(".");
   return (
-    <div>ProductPrice</div>
-  )
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">€</span>
+      {entera}
+      <span className="text-xs align-super">.{decimales}</span>
+    </p>
+  );
 }
