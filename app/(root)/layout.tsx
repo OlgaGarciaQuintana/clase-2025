510767bffabc06e3e90444527d829ca145d70067
Olga Garcia <olgarciaq11@gmail.com>
2025-11-04 09:01:04 +0100

    footer arreglado

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,12 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
    <div className="flex h-screen flex-col">
     <Header/>
     <main className="flex-1 wrapper">{children}</main>
-   </div>
     <Footer />
-   </>
+   </div>
   );
 }
