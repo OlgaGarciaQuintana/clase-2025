d22bb810c093459a2530adb6506703b55dcebe2a
Olga Garcia <olgarciaq11@gmail.com>
2025-11-04 08:41:22 +0100

    mostrar mas productos

--- components/shared/header/product/product-list.tsx
+++ components/shared/header/product/product-list.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Product } from '@/types/Product'
+import React from "react";
+import { Product } from "@/types/Product";
 
 export default function ProductList({
-    data, title, limit
-}:{
-    data: Product[];
-    title?: string;
-    limit?: number;
+  data,
+  title,
+  limit,
+}: {
+  data: Product[];
+  title?: string;
+  limit?: number;
 }) {
-    const limitedData = limit ? data.slice(0, limit) : data;
+  const limitedData = limit ? data.slice(0, limit) : data;
   return (
-    <div className='my-10'>
-        <h2 className='h2-bold mb-4'>{title?(title):"Lista Productos"}</h2>
-        {data[0].slug}
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title ? title : "Lista Productos"}</h2>
+      {limitedData.length > 0 ?
+      limitedData.map((p)=> <p key={p.slug}>{p.slug}</p>) 
+      :
+      <div>
+        <p>No products found</p>
+      </div>}
     </div>
-  )
+  );
 }
