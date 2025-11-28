6c3cc0ba7607d60569ae3f42aa69d0d944a977f2
Olga Garcia <olgarciaq11@gmail.com>
2025-11-28 09:40:47 +0100

    todo actualizado

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,14 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
+import "dotenv/config";
+import { PrismaClient, Prisma } from "@/lib/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
 import sampleData from "./sample-data";
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
 async function main() {
-    const prisma = new PrismaClient();
+    const prisma = new PrismaClient({ adapter });
     await prisma.product.createMany({data: sampleData.products});
     console.log("Productos introducidos en la base de datos");
 }
