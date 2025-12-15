6200cbcf7f74af04eb5a637c563a8da213114ef2
Olga Garcia <olgarciaq11@gmail.com>
2025-12-15 13:45:10 +0100

    nueva version y precio puesto

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,11 @@ export function convertToPlainObject<T>(value:T):T{
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//Dar formato al campo precio. Número con dos decimales como máximo
+export function formatNumberWithDecimal(num: number): string {
+  const [parte_entera,parte_decimal] = num.toString().split(".");
+  return parte_decimal
+    ? `${parte_entera}.${parte_decimal.padEnd(2,"0")}`
+    : `${parte_entera}.00`;
+}
--- types/Product.ts
+++ types/Product.ts
@@ -1,17 +1,25 @@
+import { z } from "zod";
 import { Prisma } from "@/lib/generated/prisma/client";
+import { insertProductSchema } from "@/lib/validators";
 
-export type Product = {
+// export type Product = {
+//     id?: string;
+//     name: string;
+//     slug: string;
+//     category: string;
+//     description: string;
+//     images: string[];
+//     price: number;
+//     brand: string;
+//     rating: number;
+//     numReviews: number;
+//     stock: number;
+//     isFeatured: boolean;
+//     banner: string | null;
+// };
+
+export type Product = z.infer<typeof insertProductSchema> & {
     id?: string;
-    name: string;
-    slug: string;
-    category: string;
-    description: string;
-    images: string[];
-    price: number;
-    brand: string;
-    rating: number;
-    numReviews: number;
-    stock: number;
-    isFeatured: boolean;
-    banner: string | null;
-}
+    rating: number | string;
+    createdAt?: Date;
+};
