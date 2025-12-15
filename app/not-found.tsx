316eee8e7e7b0d9ae37083211625cda805c56248
Olga Garcia <olgarciaq11@gmail.com>
2025-12-15 13:57:05 +0100

    not found arreglado

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button'
 export default function NotFound() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen'>
-        <Image src={"icon.svg"} alt={`Logo de ${APP_NAME}`} width={48} height={48} priority/>
+        <Image src={"/images/logo.svg"} alt={`Logo de ${APP_NAME}`} width={48} height={48} priority/>
         <div className='p-6 rounded-lg shadow-md text-center'>
             <h1 className='text-3xl font-bold mb-4'>Not found</h1>
             <p className='text-destructive'>Could not find the requested page</p>
