f50039b79fad5730469b22c6182e8bd7e6607eba
Olga Garcia <olgarciaq11@gmail.com>
2026-01-12 13:58:07 +0100

    better-auth terminado

---
+++ lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@/db/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+    maxPasswordLength: 1
+  },
+});
